a57726f6f636a17b3798b56b95cc0d850748db77
Geanina-Ionela Achițeni <116017251+GIA0103@users.noreply.github.com>
2023-01-08 22:31:03 +0200

    update

--- oop.p7.jpa.model/src/main/java/org/jpa/Florarie/Client.java
+++ oop.p7.jpa.model/src/main/java/org/jpa/Florarie/Client.java
@@ -1,33 +1,12 @@
 package org.jpa.Florarie;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
 
 @Entity
 public class Client extends Persoana { 
 	private static final long serialVersionUID = 1L;
-	
-	@Id
-	private Integer id;
-	private String nume;
-	
+
 	private Boolean clientFidel;
-	
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNume() {
-		return nume;
-	}
-
-	public void setNume(String nume) {
-		this.nume = nume;
-	}
 	
 	public Client(Integer id, String nume, Boolean clientFidel) {
 		super(id, nume);
